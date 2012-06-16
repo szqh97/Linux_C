53d5d216c5fe7c77eadc5187b7dffe14627e2216
szqh97 <szqh97@163.com>
2012-06-16 09:53:13 +0800

    first

---
+++ kernel/atomic.c
@@ -0,0 +1,21 @@
+/*
+ * =======================================================================
+ *       Filename:  atomic.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2012年04月25日 21时43分59秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  szqh97 (), szqh97@163.com
+ *        Company:  szqh97
+ * =======================================================================
+ */
+#include "apue.h"
+#include <asm/atomic.h>
+
+int main ( int argc, char *argv[] )
+{
+    return 0;
+}	/* ----------  end of function main  ---------- */
+
+
